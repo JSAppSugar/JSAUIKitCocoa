8ad9a84a097fbe06f688e8cf14cde72316f2f5f2
Neal <>
2018-11-13 22:48:29 +0800

    Initial commit

---
+++ JSAUIKitCocoaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSAUIKitCocoaDemo
+//
+//  Created by Neal on 2018/11/13.
+//  Copyright © 2018年 JSAppSugar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSAUIKitCocoaDemoUITests/JSAUIKitCocoaDemoUITests.m
@@ -0,0 +1,38 @@
+//
+//  JSAUIKitCocoaDemoUITests.m
+//  JSAUIKitCocoaDemoUITests
+//
+//  Created by Neal on 2018/11/13.
+//  Copyright © 2018年 JSAppSugar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSAUIKitCocoaDemoUITests : XCTestCase
+
+@end
+
+@implementation JSAUIKitCocoaDemoUITests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
